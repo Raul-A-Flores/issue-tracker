e7409c0991b903a64cbe2cd2634df3851807b22b
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-11-07 13:09:46 -0800

    set up prisma

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,15 +1,40 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
 
 type Props = {}
 
 const NavBar = (props: Props) => {
+
+    const currentPath = usePathname()
+
+    const links = [
+        {label: 'Dashboard', href: '/'},
+        {label: 'Issues', href: '/issues'},
+
+    ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'>Logo</Link>
+        <Link href='/'><AiFillBug /></Link>
         <ul className='flex space-x-6'>
-            <li><Link href='/'>Dashboard</Link></li>
-            <li><Link href='/issues'>Issues</Link></li>
+            {links.map(link =>
+                <Link 
+                key={link.href} 
+               // className={` ${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+                href={link.href}
+                className={classnames({
+                    'text-zinc-900' : link.href === currentPath,
+                    'text-zinc-500' : link.href !== currentPath,
+                    'hover:text-zinc-800 transition-colors' : true,
+                })}
+                >
+                {link.label}</Link>
+            )}
+       
         </ul>
     </nav>
   )
---
+++ src/app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const IssuesPage = (props: Props) => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default IssuesPage
